d3a6ce7b96af9d1f92ecf2cb1f11ad04e420d000
vindexTOS <giorgikutateladze1998@gmail.com>
2024-08-15 12:17:51 +0400

    login page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import LandingPage from "@/components/landing-page";
+ 
+// import LogInForm from "@/components/auth/login-form";
+import LoginPage from "./auth/(login)/login3/page";
 
 const page = () => {
-  return <LandingPage />;
+  return < LoginPage />;
 };
 
 export default page;
